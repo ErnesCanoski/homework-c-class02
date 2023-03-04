d60d9220e146be201568a4239e4c4f59d74dac69
ErnesCanoski <119903138+ErnesCanoski@users.noreply.github.com>
2023-03-04 05:12:29 +0100

    Add files via upload

---
+++ Homework/homework.class02/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//exercise 7
+
+Console.Write("Enter a number from 1 to 3: ");
+string input = Console.ReadLine();
+int number;
+
+if (int.TryParse(input, out number))
+{
+    switch (number)
+    {
+        case 1:
+            Console.WriteLine("You got a new car!");
+            break;
+        case 2:
+            Console.WriteLine("You got a new plane!");
+            break;
+        case 3:
+            Console.WriteLine("You got a new bike!");
+            break;
+        default:
+            Console.WriteLine("Error: Invalid input. Please enter a number from 1 to 3.");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Error: Invalid input. Please enter a number from 1 to 3.");
+}
+
+Console.ReadLine();
